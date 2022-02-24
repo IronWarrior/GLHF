9ba87d2a476066313d217f35be30a31e30df98a5
Erik <erikroystan@gmail.com>
2022-02-24 13:22:29 -0800

    Add TODO.

--- Assets/Source/State/StateInput.cs
+++ Assets/Source/State/StateInput.cs
@@ -1,5 +1,7 @@
 namespace GLHF
 {
+    // TODO: Can just use unsafe to allow the user to define
+    // their own input types.
     [System.Serializable]
     public partial struct StateInput
     {
